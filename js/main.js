ae43cd48521e05f315ab7af11201b03ab23e94b3
rebs-code <jack.rebussi@gmail.com>
2023-11-15 17:42:13 +0100

    feat: add resetBoard function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 "use strict";
 
 // functions
+
+//funzione crea elementi
 function createElement(tag, className, content) {
     const element = document.createElement(tag);
     element.classList.add(className);
     element.append(content);
     return element;
+}
+
+//funzione resetta board
+function resetBoard() {
+    document.getElementById('board').innerHTML = '';
 }
 
 // execution
@@ -14,7 +21,7 @@ const playButton = document.getElementById("play");
 
 
 playButton.addEventListener("click", function () {
-    
+    resetBoard();
     for (let i = 1; i <= 100; i++) {
         const myElement = createElement("div", "cell", i);
         myElement.addEventListener("click", function() {
