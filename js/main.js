1254320b0d7c7baf16dbb7f141088693e5fe80e2
rebs-code <jack.rebussi@gmail.com>
2023-11-15 17:57:57 +0100

    feat: Add cell number based on difficulty level

--- js/main.js
+++ js/main.js
@@ -22,13 +22,35 @@ const playButton = document.getElementById("play");
 
 playButton.addEventListener("click", function () {
     resetBoard();
-    for (let i = 1; i <= 100; i++) {
-        const myElement = createElement("div", "cell", i);
+    //predo il valore del select
+    const inputValue = document.getElementById("difficolta").value;
+    if (inputValue === "1") {
+        for (let i = 1; i <= 49; i++) {
+        const myElement = createElement("div", "cell-7", i);
         myElement.addEventListener("click", function() {
             console.log(`Cell ${i} clicked!`);
             myElement.classList.toggle("blue");
         });
         document.getElementById('board').append(myElement);
+    }
+    } else if (inputValue === "2") {
+        for (let i = 1; i <= 81; i++) {
+            const myElement = createElement("div", "cell-9", i);
+            myElement.addEventListener("click", function() {
+                console.log(`Cell ${i} clicked!`);
+                myElement.classList.toggle("blue");
+            });
+            document.getElementById('board').append(myElement);
+        }
+    } else if (inputValue === "3") {
+        for (let i = 1; i <= 100; i++) {
+            const myElement = createElement("div", "cell-10", i);
+            myElement.addEventListener("click", function() {
+                console.log(`Cell ${i} clicked!`);
+                myElement.classList.toggle("blue");
+            });
+            document.getElementById('board').append(myElement);
+        }
     }
 });
 
