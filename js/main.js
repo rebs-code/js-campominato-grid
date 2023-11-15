9557a2d01e22b95a7f217527183dcb88d240f961
rebs-code <jack.rebussi@gmail.com>
2023-11-15 17:58:30 +0100

    style: format js

--- js/main.js
+++ js/main.js
@@ -4,53 +4,51 @@
 
 //funzione crea elementi
 function createElement(tag, className, content) {
-    const element = document.createElement(tag);
-    element.classList.add(className);
-    element.append(content);
-    return element;
+  const element = document.createElement(tag);
+  element.classList.add(className);
+  element.append(content);
+  return element;
 }
 
 //funzione resetta board
 function resetBoard() {
-    document.getElementById('board').innerHTML = '';
+  document.getElementById("board").innerHTML = "";
 }
 
 // execution
 
 const playButton = document.getElementById("play");
 
-
 playButton.addEventListener("click", function () {
-    resetBoard();
-    //predo il valore del select
-    const inputValue = document.getElementById("difficolta").value;
-    if (inputValue === "1") {
-        for (let i = 1; i <= 49; i++) {
-        const myElement = createElement("div", "cell-7", i);
-        myElement.addEventListener("click", function() {
-            console.log(`Cell ${i} clicked!`);
-            myElement.classList.toggle("blue");
-        });
-        document.getElementById('board').append(myElement);
+  resetBoard();
+  //predo il valore del select
+  const inputValue = document.getElementById("difficolta").value;
+  if (inputValue === "1") {
+    for (let i = 1; i <= 49; i++) {
+      const myElement = createElement("div", "cell-7", i);
+      myElement.addEventListener("click", function () {
+        console.log(`Cell ${i} clicked!`);
+        myElement.classList.toggle("blue");
+      });
+      document.getElementById("board").append(myElement);
     }
-    } else if (inputValue === "2") {
-        for (let i = 1; i <= 81; i++) {
-            const myElement = createElement("div", "cell-9", i);
-            myElement.addEventListener("click", function() {
-                console.log(`Cell ${i} clicked!`);
-                myElement.classList.toggle("blue");
-            });
-            document.getElementById('board').append(myElement);
-        }
-    } else if (inputValue === "3") {
-        for (let i = 1; i <= 100; i++) {
-            const myElement = createElement("div", "cell-10", i);
-            myElement.addEventListener("click", function() {
-                console.log(`Cell ${i} clicked!`);
-                myElement.classList.toggle("blue");
-            });
-            document.getElementById('board').append(myElement);
-        }
+  } else if (inputValue === "2") {
+    for (let i = 1; i <= 81; i++) {
+      const myElement = createElement("div", "cell-9", i);
+      myElement.addEventListener("click", function () {
+        console.log(`Cell ${i} clicked!`);
+        myElement.classList.toggle("blue");
+      });
+      document.getElementById("board").append(myElement);
     }
+  } else if (inputValue === "3") {
+    for (let i = 1; i <= 100; i++) {
+      const myElement = createElement("div", "cell-10", i);
+      myElement.addEventListener("click", function () {
+        console.log(`Cell ${i} clicked!`);
+        myElement.classList.toggle("blue");
+      });
+      document.getElementById("board").append(myElement);
+    }
+  }
 });
-
